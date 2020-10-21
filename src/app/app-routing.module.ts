b69ddfce25475621939e4a6af7239efd5d86e610
Sravan Kadium <sravankadium@Sravans-MacBook-Pro.local>
2020-10-21 10:13:48 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,26 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'tab1',
+    loadChildren: () => import('./tab1/tab1.module').then( m => m.Tab1PageModule)
+  },
+  {
+    path: 'tab2',
+    loadChildren: () => import('./tab2/tab2.module').then( m => m.Tab2PageModule)
+  },
+  {
+    path: 'side-menu',
+    loadChildren: () => import('./side-menu/side-menu.module').then( m => m.SideMenuPageModule)
+  },
+  {
+    path: 'menu1',
+    loadChildren: () => import('./menu1/menu1.module').then( m => m.Menu1PageModule)
+  },
+  {
+    path: 'menu2',
+    loadChildren: () => import('./menu2/menu2.module').then( m => m.Menu2PageModule)
+  },
 ];
 
 @NgModule({
